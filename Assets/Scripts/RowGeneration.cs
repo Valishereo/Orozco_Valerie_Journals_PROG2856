775eec75c83507a7522f21cf175facb94a2de211
orozcolv <orozcolv@OA-S138-20>
2026-09-14 01:39:32 -0400

    Started Task2, created button and the input for the numbers of squares

---
+++ Assets/Scripts/RowGeneration.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+public class RowGeneration : MonoBehaviour
+{
+    public TMP_InputField squareNumberInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateRow()
+    {
+        Debug.Log("Generate button clicked");
+    }
+}
