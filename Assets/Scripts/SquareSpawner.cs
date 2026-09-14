1ab5cd2d975f09a6ee2029366e6f7e2b1db4c44a
orozcolv <orozcolv@OA-S138-20>
2026-09-13 23:57:33 -0400

    When clicked white square appears in the clicked pos

--- Assets/Scripts/SquareSpawner.cs
+++ Assets/Scripts/SquareSpawner.cs
@@ -5,6 +5,7 @@ public class SquareSpawner : MonoBehaviour
 {
     public float squareSize = 2f;
     Vector2 clickedPosition;
+    bool hasClicked = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,10 +15,35 @@ public class SquareSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Tracking mouse position in world space
+
         Vector2 mousePosition = Mouse.current.position.ReadValue();
         Vector3 screenPosition = new Vector3(mousePosition.x, mousePosition.y, 10f);
         Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
         //Vector2 endPosition = new Vector2(worldPosition.x + 1f, worldPosition.y);
+
+        if (Mouse.current.leftButton.wasPressedThisFrame) //To draw white square when mouse is clicked
+        {
+            clickedPosition = worldPosition;
+            hasClicked = true;
+        }
+
+        //White square drawing in clicked position
+
+        if (hasClicked)
+        {
+            Vector2 clickedtopLeft = new Vector2(clickedPosition.x - squareSize / 2, clickedPosition.y + squareSize / 2);
+            Vector2 clickedtopRight = new Vector2(clickedPosition.x + squareSize / 2, clickedPosition.y + squareSize / 2);
+            Vector2 clickedbottomLeft = new Vector2(clickedPosition.x - squareSize / 2, clickedPosition.y - squareSize / 2);
+            Vector2 clickedbottomRight = new Vector2(clickedPosition.x + squareSize / 2, clickedPosition.y - squareSize / 2);
+
+            Debug.DrawLine(clickedtopLeft, clickedtopRight, Color.white);
+            Debug.DrawLine(clickedtopRight, clickedbottomRight, Color.white);
+            Debug.DrawLine(clickedbottomRight, clickedbottomLeft, Color.white);
+            Debug.DrawLine(clickedbottomLeft, clickedtopLeft, Color.white);
+        }
+
+        //Square drawing semiTransparnet
 
         Vector2 topLeft = new Vector2(worldPosition.x - squareSize / 2, worldPosition.y + squareSize / 2);
         Vector2 topRight = new Vector2(worldPosition.x + squareSize / 2, worldPosition.y + squareSize / 2);
@@ -26,7 +52,7 @@ public class SquareSpawner : MonoBehaviour
 
         //Debug.DrawLine(worldPosition, endPosition, Color.white);
 
-        Color sTWhite = new Color(1f, 1f, 1f, 0.3f);
+        Color sTWhite = new Color(1f, 1f, 1f, 0.3f); //Color white with 30% opacity
 
         Debug.DrawLine(topLeft, topRight, sTWhite);
         Debug.DrawLine(topRight, bottomRight, sTWhite);
