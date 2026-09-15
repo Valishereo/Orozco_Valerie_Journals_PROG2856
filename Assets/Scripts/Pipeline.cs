a443d27fdbe1e8b2c15a8094d71ee959cd8539f4
orozcolv <orozcolv@OA-S141-17>
2026-09-14 20:49:32 -0400

    Calculate mouse pos, when mouse is pressed a line will be drawn every 0.1 secs

--- Assets/Scripts/Pipeline.cs
+++ Assets/Scripts/Pipeline.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class Pipeline : MonoBehaviour
 {
+    Vector2 previousPosition;
+    float timer = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,26 @@ public class Pipeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 mousePosition = Mouse.current.position.ReadValue(); // Get the current mouse position in screen coordinates
+        Vector3 screenPosition = new Vector3(mousePosition.x, mousePosition.y, 10f);
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            previousPosition = worldPosition;
+            timer = 0f;
+        }
+
+        if (Mouse.current.leftButton.isPressed)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 0.1f) // Adjust the threshold as needed
+            {
+                Debug.DrawLine(previousPosition, worldPosition, Color.white, 100f);
+                previousPosition = worldPosition;
+                timer = 0f;
+            }
+        }
     }
 }
