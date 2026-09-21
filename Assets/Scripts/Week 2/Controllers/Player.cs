0fb14e4c7fd9c94a4c44dfaf6781d4e313f7883e
orozcolv <orozcolv@OA-S138-20>
2026-09-20 21:02:09 -0400

    Task #2, bombs now spawn in random corners from the player when r is pressed

--- Assets/Scripts/Week 2/Controllers/Player.cs
+++ Assets/Scripts/Week 2/Controllers/Player.cs
@@ -17,18 +17,23 @@ public class Player : MonoBehaviour
         Debug.Log(NormalizeVector(new Vector3(-3, 4)));
         Debug.Log(NormalizeVector(new Vector3(1.5f, -3.5f)));
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         if (Keyboard.current.bKey.wasPressedThisFrame)
-            {
-            SpawnBombAtOffSet(bombOffset); // Spawn a bomb at the specified offset from the player's position
-            }
+        {
+            SpawnBombAtOffSet(bombOffset); // Spawn a bomb at the specified offset from the player's position when B is pressed
+        }
 
         if (Keyboard.current.tKey.wasPressedThisFrame) // It spawns a trail of bombs behind the player when T is pressed
         {
             SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
+
+        if (Keyboard.current.rKey.wasPressedThisFrame) // It spawns a bomb at a random corner when R is pressed
+        {
+            SpawnBombOnRandomCorner(3f);
         }
     }
     public void SpawnBombAtOffSet(Vector3 inOffset)
@@ -47,6 +52,32 @@ public class Player : MonoBehaviour
             Vector3 offset = -transform.up * distance; 
             SpawnBombAtOffSet(offset);
         }
+    }
+
+    public void SpawnBombOnRandomCorner(float inDistance)
+    {
+        int randomCorner = Random.Range(0, 4); // Randomly select a corner 
+        Vector3 direction = Vector3.zero;
+        
+        switch (randomCorner) 
+        {
+            case 0:
+                direction = transform.up + transform.right; // Top-right corner
+                break;
+            case 1:
+                direction = transform.up - transform.right; // Top-left corner
+                break;
+            case 2:
+                direction = -transform.up + transform.right; // Bottom-right corner
+                break;
+            case 3:
+                direction = -transform.up - transform.right; // Bottom-left corner
+                break;
+        }
+
+        direction =NormalizeVector(direction);
+        Vector3 offset = direction * inDistance; // Calculate the offset based on the distance and direction
+        SpawnBombAtOffSet(offset); // Spawn the bomb at the calculated offset
     }
 
     Vector2 NormalizeVector(Vector2 inVector)
